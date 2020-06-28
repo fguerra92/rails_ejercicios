88863457ee499903199cb6b53bc4ddc68b6e8a4e
Felipe Guerra <fguerra.ag@gmail.com>
2020-06-27 22:07:43 -0400

    first commit

---
+++ clase.rb
@@ -0,0 +1,12 @@
+class Persona
+    def saludar
+        puts "hola!!"
+    end
+end
+
+# 2.5.3 :006 > persona1 = Persona.new
+#  => #<Persona:0x0000560e7b297888> 
+# 2.5.3 :007 > persona1.saludar
+# hola!!
+#  => nil 
+# 2.5.3 :008 > 
---
+++ perro.rb
@@ -0,0 +1,12 @@
+class Perro
+    def ladrar
+        puts 'bark'
+    end
+end
+
+sparky = Perro.new
+fluffy = Perro.new
+
+sparky.ladrar
+fluffy.ladrar
+
